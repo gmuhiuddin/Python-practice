4b62803df00fc03102c48eae04c8b86a051760e0
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-11-10 17:39:37 +0500

    add initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("bla bla bla")
+
+txts = "gmuhiuddin"
+
+# for txt in txts: 
+#     print(txt)
+
+# Python data types
+
+# 1. "" => str
+# 2. 1 => int
+# 3. 1.2 => float
+# 4. True / False => bool
+
+# txtsList = ["txt1", "txt2"]
+
+# print(txtsList)
+
+# print(txtsList.index("txt2"))
+
+# print(txtsList)
+
+# if False:
+#     print("true")
+# else:
+#     print("false")
+
